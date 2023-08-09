cae39f131feff1dc63d396fafcccb2a55d5feb51
eliflores <eli@serlo.org>
2023-08-09 18:44:05 +0200

    Add tests for convert2rss.py
    And refactor the code to test the generate_rss function.

--- convert2rss.py
+++ convert2rss.py
@@ -3,6 +3,8 @@
 import json
 import sys
 from datetime import datetime
+from typing import Dict, Any
+
 from serlo_api_client import fetch_publisher
 
 
@@ -11,79 +13,70 @@ def main(input_filename: str, output_filename: str):
         metadata = json.load(input_file)
 
     with open(output_filename, "w", encoding="utf-8") as output_file:
-        convert(output_file, metadata)
+        publisher = get_publisher()
+        rss_export = generate_rss(metadata, publisher, datetime.utcnow())
+        print(rss_export, file=output_file)
 
 
-def convert(file, metadata):
-    publisher = get_publisher()
-
+def generate_rss(
+    metadata: Dict[str, Any], publisher: Dict[str, Any], published_date: datetime
+) -> str:
     serlo_url = escape(publisher["url"])
     serlo_description = escape(publisher["description"])
     serlo_name = escape(publisher["name"])
 
-    print('<?xml version="1.0" encoding="UTF-8"?>', file=file)
-    print(
-        '<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:sdx="sodix.dtd" version="2.0">',
-        file=file,
-    )
-    print("<channel>", file=file)
-    print(f"  <title>{serlo_name}</title>", file=file)
-    print(f"  <link>{serlo_url}</link>", file=file)
-    print(f"  <description>{serlo_description}</description>", file=file)
-    print("  <language>de-DE</language>", file=file)
-    print(f"  <copyright>{serlo_name}</copyright>", file=file)
-    print(f"  <pubDate>{format_date(datetime.utcnow())}</pubDate>", file=file)
+    rss = """<?xml version="1.0" encoding="UTF-8"?>
+<rss xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:sdx="sodix.dtd" version="2.0">
+<channel>
+"""
+
+    rss += f"  <title>{serlo_name}</title>\n"
+    rss += f"  <link>{serlo_url}</link>\n"
+    rss += f"  <description>{serlo_description}</description>\n"
+    rss += "  <language>de-DE</language>\n"
+    rss += f"  <copyright>{serlo_name}</copyright>\n"
+    rss += f"  <pubDate>{format_date(published_date)}</pubDate>\n"
 
     for resource in metadata:
-        convert_resource(file, resource, publisher)
+        rss += converted_resource(resource, publisher)
 
-    print("</channel>", file=file)
-    print("</rss>", file=file)
+    rss += """</channel>
+</rss>
+"""
+
+    return rss
 
 
-def convert_resource(file, resource, publisher):
-    print("<item>", file=file)
-    print(f'  <title>{escape(resource["name"])}</title>', file=file)
-    print(
-        f'  <sdx:language>{escape(resource["inLanguage"][0])}</sdx:language>', file=file
-    )
+def converted_resource(resource: Dict[str, Any], publisher: Dict[str, Any]) -> str:
+    rss = "<item>\n"
+
+    rss += f'  <title>{escape(resource["name"])}</title>\n'
+    rss += f'  <sdx:language>{escape(resource["inLanguage"][0])}</sdx:language>\n'
 
     if "description" in resource and resource["description"]:
-        print(
-            f'  <description>{escape(resource["description"])}</description>', file=file
-        )
+        rss += f'  <description>{escape(resource["description"])}</description>\n'
 
-    print(f'  <link>{escape(resource["id"])}</link>', file=file)
+    rss += f'  <link>{escape(resource["id"])}</link>\n'
 
     authors = ", ".join(escape(author["name"]) for author in resource["creator"])
 
-    print(f"  <author>{authors}</author>", file=file)
-    print(
-        f'  <sdx:authorsWebsite>{escape(publisher["url"])}</sdx:authorsWebsite>',
-        file=file,
-    )
-    print(f'  <sdx:producer>{escape(publisher["name"])}</sdx:producer>', file=file)
+    rss += f"  <author>{authors}</author>\n"
+    rss += f'  <sdx:authorsWebsite>{escape(publisher["url"])}</sdx:authorsWebsite>\n'
 
-    print(f'  <guid isPermaLink="true">{escape(resource["id"])}</guid>', file=file)
+    rss += f'  <sdx:producer>{escape(publisher["name"])}</sdx:producer>\n'
+    rss += f'  <guid isPermaLink="true">{escape(resource["id"])}</guid>\n'
 
     date_created = format_date(datetime.fromisoformat(resource["dateCreated"]))
-    print(f"  <pubDate>{date_created}</pubDate>", file=file)
+    rss += f"  <pubDate>{date_created}</pubDate>\n"
 
-    print("  <sdx:userGroups>learner, teacher</sdx:userGroups>", file=file)
-    print(
-        "  <sdx:educationalLevel>Sekundarstufe I, Sekundarstufe II</sdx:educationalLevel>",
-        file=file,
-    )
-    print("  <sdx:classLevel>5-13</sdx:classLevel>", file=file)
+    rss += """  <sdx:userGroups>learner, teacher</sdx:userGroups>
+  <sdx:educationalLevel>Sekundarstufe I, Sekundarstufe II</sdx:educationalLevel>
+  <sdx:classLevel>5-13</sdx:classLevel>
+"""
 
-    print(
-        f"  <sdx:learnResourceType>{escape(get_resource_type(resource))}</sdx:learnResourceType>",
-        file=file,
-    )
-    print(
-        "  <sdx:schoolType>Realschule, Gymnasium, Mittel- / Hauptschule, Fachoberschule</sdx:schoolType>",
-        file=file,
-    )
+    rss += f"  <sdx:learnResourceType>{escape(get_resource_type(resource))}</sdx:learnResourceType>\n"
+
+    rss += "  <sdx:schoolType>Realschule, Gymnasium, Mittel- / Hauptschule, Fachoberschule</sdx:schoolType>\n"
 
     # TODO # pylint: disable=W0511
     # print('  <category domain="eaf-classification-coded">38002</category>', file=file)
@@ -93,19 +86,19 @@ def convert_resource(file, resource, publisher):
     license_name = get_license_name(resource_license)
 
     if license_name:
-        print(f"  <sdx:licenseName>{escape(license_name)}</sdx:licenseName>", file=file)
-        print("  <sdx:licenseCountry>de</sdx:licenseCountry>", file=file)
+        rss += f"  <sdx:licenseName>{escape(license_name)}</sdx:licenseName>\n"
+        rss += "  <sdx:licenseCountry>de</sdx:licenseCountry>\n"
 
         version = get_license_version(resource_license)
 
         if version:
-            print(
-                f"  <sdx:licenseVersion>{escape(version)}</sdx:licenseVersion>",
-                file=file,
-            )
+            rss += f"  <sdx:licenseVersion>{escape(version)}</sdx:licenseVersion>\n"
 
-    print("  <sdx:costs>FREE</sdx:costs>", file=file)
-    print("</item>", file=file)
+    rss += """  <sdx:costs>FREE</sdx:costs>
+</item>
+"""
+
+    return rss
 
 
 def get_license_version(resource_license):
@@ -155,7 +148,7 @@ def format_date(date):
     return escape(date.strftime("%a, %d %b %Y %H:%M:%S +0000"))
 
 
-def get_publisher():
+def get_publisher() -> Dict[str, Any]:
     response = fetch_publisher()
     return response["metadata"]["publisher"]  # pylint: disable=E1136
 
