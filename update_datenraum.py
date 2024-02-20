16658141e7777faa31ce51528624718fd6ebb3b9
Stephan Kulla <git.mail@kulla.me>
2024-02-20 23:03:40 +0100

    feat: Add only nodes which haven't been added before

--- update_datenraum.py
+++ update_datenraum.py
@@ -4,25 +4,39 @@ import json
 from datenraum import create_datenraum_session
 
 
-def main(metadata_file):
+def main(metadata_file, nodes_file):
     with open(metadata_file, "r", encoding="utf-8") as input_file:
         ressources = json.load(input_file)
 
+    datenraum_id_to_serlo_id = {}
+
+    with open(nodes_file, "r", encoding="utf-8") as input_file:
+        nodes = json.load(input_file)
+
+        for node in nodes:
+            datenraum_id_to_serlo_id[node["externalId"]] = node["id"]
+
     session = create_datenraum_session()
 
-    for ressource in ressources[:1000]:
-        try:
-            session.add_node(ressource)
-        except AssertionError:
-            # TODO: Update ressource
-            print(ressource["id"])
+    for ressource in ressources[:1010]:
+        ressource_id = ressource["id"]
+        datenraum_id = datenraum_id_to_serlo_id.get(ressource["id"], None)
 
-    print(session.token["access_token"])
+        if datenraum_id is None:
+            print(f"INFO: Add new node for {ressource_id}")
+
+            # Add a new node
+            try:
+                session.add_node(ressource)
+            except AssertionError:
+                print(f"ERROR: {ressource_id} couldn't be added")
+        else:
+            pass
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: update_datenraum.py SERLO_METADATA_FILE")
+    if len(sys.argv) < 3:
+        print("Usage: update_datenraum.py SERLO_METADATA_FILE DATENRAUM_NODES_FILE")
         sys.exit(1)
 
-    main(sys.argv[1])
+    main(sys.argv[1], sys.argv[2])
