57b772f73d8bd5551cc434880473a68c2f3e47be
MoeHome <moritz+home@serlo.org>
2024-03-13 16:33:19 +0100

    format

--- add_taxonomy_to_datenraum.py
+++ add_taxonomy_to_datenraum.py
@@ -1,11 +1,13 @@
 from datenraum import create_datenraum_session
 from taxonomy_node_examples import BIG_DATA_TAXONOMY, GAME_THEORY_TAXONOMY
 
+
 def main():
     session = create_datenraum_session()
-    #session.add_node(BIG_DATA_TAXONOMY, "Taxonomy")
-    #session.update_node(BIG_DATA_TAXONOMY, "04141a5e-cc07-4508-af38-703b42680149", "Taxonomy")
-    #session.delete_node("d8f481cb-3be6-4a91-9f5b-c22b35b918ae")
+    session.add_node(BIG_DATA_TAXONOMY, "Taxonomy")
+    # session.update_node(BIG_DATA_TAXONOMY, "04141a5e-cc07-4508-af38-703b42680149", "Taxonomy")
+    # session.delete_node("d8f481cb-3be6-4a91-9f5b-c22b35b918ae")
     session.add_node(GAME_THEORY_TAXONOMY, "Taxonomy")
+
 
 main()
--- taxonomy_node_examples.py
+++ taxonomy_node_examples.py
@@ -1,105 +1,93 @@
 BIG_DATA_TAXONOMY = {
-	"about": [
+    "about": [
         {
-          "type": "Concept",
-          "id": "http://w3id.org/kim/schulfaecher/s1013",
-          "inScheme": {
-            "id": "http://w3id.org/kim/schulfaecher/"
-          }
+            "type": "Concept",
+            "id": "http://w3id.org/kim/schulfaecher/s1013",
+            "inScheme": {"id": "http://w3id.org/kim/schulfaecher/"},
         }
-      ],
-	"@context":[
-		"https://w3id.org/kim/amb/context.jsonld",
-		"https://schema.org",
     ],
-	"dateCreated": "2020-03-17T09:33:15",
-	"dateModified": "2020-04-30T07:48:36",
-	"creator": [
-		{
-			"type": "Person",
-			"id": "https://serlo.org/18981",
-          	"name": "wolfgang",
-		}
-	],
-	"description": "In diesem Themenbereich auf Serlo findest du drei kurze Artikel, die den Begriff „Big Data“ einfach und anschaulich erklären – die Links dazu findest du unten, am Ende der Einleitung. Viel Spaß!",
-	"headline": "Big Data – einfach erklärt",
-	"id": "https://serlo.org/155948",
-	"identifier": {
-		"propertyID": "UUID",
-        "type": "PropertyValue",
-        "value": 155948
-	},
-	"image": "https://assets.serlo.org/5e6d30a60f21a_33bd694cc1e54c7be4a3363277ce1aeec8c5d0c0.jpg",
-	"inLanguage": [
-        "de"
+    "@context": [
+        "https://w3id.org/kim/amb/context.jsonld",
+        "https://schema.org",
     ],
-	"isAccessibleForFree": True,
+    "dateCreated": "2020-03-17T09:33:15",
+    "dateModified": "2020-04-30T07:48:36",
+    "creator": [
+        {
+            "type": "Person",
+            "id": "https://serlo.org/18981",
+            "name": "wolfgang",
+        }
+    ],
+    "description": """In diesem Themenbereich auf Serlo findest du drei kurze Artikel,
+     die den Begriff „Big Data“ einfach und anschaulich erklären –
+      die Links dazu findest du unten, am Ende der Einleitung. Viel Spaß!""",
+    "headline": "Big Data – einfach erklärt",
+    "id": "https://serlo.org/155948",
+    "identifier": {"propertyID": "UUID", "type": "PropertyValue", "value": 155948},
+    "image": "https://assets.serlo.org/5e6d30a60f21a_33bd694cc1e54c7be4a3363277ce1aeec8c5d0c0.jpg",
+    "inLanguage": ["de"],
+    "isAccessibleForFree": True,
     "isFamilyFriendly": True,
-	"maintainer": {
+    "maintainer": {
         "id": "https://serlo.org/organization",
         "name": "Serlo Education e.V.",
-        "type": "Organization"
-	},
+        "type": "Organization",
+    },
     "name": "Big Data – einfach erklärt",
     "publisher": [
-		{
-			"id": "https://serlo.org/organization",
-			"name": "Serlo Education e.V.",
-			"type": "Organization"
-		}
-	],
-	"type": ["LearningResource"]
+        {
+            "id": "https://serlo.org/organization",
+            "name": "Serlo Education e.V.",
+            "type": "Organization",
+        }
+    ],
+    "type": ["LearningResource"],
 }
 
 GAME_THEORY_TAXONOMY = {
-	"about": [
+    "about": [
         {
-          "type": "Concept",
-          "id": "http://w3id.org/kim/schulfaecher/s1013",
-          "inScheme": {
-            "id": "http://w3id.org/kim/schulfaecher/"
-          }
+            "type": "Concept",
+            "id": "http://w3id.org/kim/schulfaecher/s1013",
+            "inScheme": {"id": "http://w3id.org/kim/schulfaecher/"},
         }
-      ],
-	"@context":[
-		"https://w3id.org/kim/amb/context.jsonld",
-		"https://schema.org",
     ],
-	"dateCreated": "2021-02-04T20:08:55",
-	"dateModified": "2021-02-04T20:13:40",
-	"creator": [
-		{
-			"type": "Person",
-			"id": "https://serlo.org/18981",
-          	"name": "wolfgang",
-		}
-	],
-	"description": "Spieltheorie ist eine mathematische Disziplin, die das rationale Verhalten von Entscheidungsträgern in interaktiven Situationen analysiert, in denen die Ergebnisse von den Entscheidungen anderer abhängen.",
-	"headline": "Spieltheorie",
-	"id": "https://serlo.org/192971",
-	"identifier": {
-		"propertyID": "UUID",
-        "type": "PropertyValue",
-        "value": 192971
-	},
-	"image": "https://assets.serlo.org/5e6d30a60f21a_33bd694cc1e54c7be4a3363277ce1aeec8c5d0c0.jpg",
-	"inLanguage": [
-        "de"
+    "@context": [
+        "https://w3id.org/kim/amb/context.jsonld",
+        "https://schema.org",
     ],
-	"isAccessibleForFree": True,
+    "dateCreated": "2021-02-04T20:08:55",
+    "dateModified": "2021-02-04T20:13:40",
+    "creator": [
+        {
+            "type": "Person",
+            "id": "https://serlo.org/18981",
+            "name": "wolfgang",
+        }
+    ],
+    "description": """Spieltheorie ist eine mathematische Disziplin,
+     die das rationale Verhalten von Entscheidungsträgern in interaktiven Situationen analysiert,
+      in denen die Ergebnisse von den Entscheidungen anderer abhängen.""",
+    "headline": "Spieltheorie",
+    "id": "https://serlo.org/192971",
+    "identifier": {"propertyID": "UUID", "type": "PropertyValue", "value": 192971},
+    "image": "https://assets.serlo.org/5e6d30a60f21a_33bd694cc1e54c7be4a3363277ce1aeec8c5d0c0.jpg",
+    "inLanguage": ["de"],
+    "isAccessibleForFree": True,
     "isFamilyFriendly": True,
-	"maintainer": {
+    "maintainer": {
         "id": "https://serlo.org/organization",
         "name": "Serlo Education e.V.",
-        "type": "Organization"
-	},
+        "type": "Organization",
+    },
     "name": "Spieltheorie",
     "publisher": [
-		{
-			"id": "https://serlo.org/organization",
-			"name": "Serlo Education e.V.",
-			"type": "Organization"
-		}
-	],
-	"type": ["LearningResource"]
+        {
+            "id": "https://serlo.org/organization",
+            "name": "Serlo Education e.V.",
+            "type": "Organization",
+        }
+    ],
+    "type": ["LearningResource"],
 }
