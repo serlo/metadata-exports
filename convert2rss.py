f99b0ab12198547dc48969396f7cf039c9a2c2ce
Stephan Kulla <git.mail@kulla.me>
2024-05-06 19:26:13 +0200

    fix(convert2rss): Filter out all elements without a description

--- convert2rss.py
+++ convert2rss.py
@@ -85,6 +85,7 @@ def filtered_data(metadata: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
         res
         for res in metadata
         if is_in_german(res)
+        and has_description(res)
         and not is_a_video_resource(res)
         and not is_a_quiz_resource(res)
         and is_the_subject_math(res)
