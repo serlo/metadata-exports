6f7498d2b1deab314843f715231d97c2bf69e044
Hugo Tiburtino <45924645+hugotiburtino@users.noreply.github.com>
2025-01-27 03:52:47 +0100

    feat(content): write the content to file as cache

--- download_metadata.py
+++ download_metadata.py
@@ -5,7 +5,7 @@ import sys
 import time
 
 from datenraum import get_current_environment, Environment
-from serlo_api_client import fetch_entity_content, fetch_metadata
+from serlo_api_client import fetch_metadata
 
 
 def main(output_filename: str):
@@ -13,17 +13,7 @@ def main(output_filename: str):
 
     print(f"INFO: {len(records)} metadata records downloaded")
 
-    if get_current_environment() == Environment.POSTDAM:
-        for record in records:
-            serlo_uuid = int(record["id"].replace("https://serlo.org/", ""))
-            print(f"fetching content of {serlo_uuid}")
-            response = fetch_entity_content(serlo_uuid)
-            if "currentRevision" in response["uuid"] and (
-                "content" in response["uuid"]["currentRevision"]
-            ):
-                record["content"] = response["uuid"]["currentRevision"]["content"]
-            # Let's not DDOS our own api
-            time.sleep(0.1)
+    # TODO: check json file from notebook for content
 
     with open(output_filename, "w", encoding="utf-8") as output_file:
         json.dump(records, output_file)
