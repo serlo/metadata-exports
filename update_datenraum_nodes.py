ad7619f79ca5979d4c64bab2e054dcef9e094477
Stephan Kulla <git.mail@kulla.me>
2025-02-06 19:51:55 +0100

    refactor: Simplify deletion of nodes

--- update_datenraum_nodes.py
+++ update_datenraum_nodes.py
@@ -161,11 +161,7 @@ def update_session(session, records, datenraum_nodes):
 
 
 def delete_deprecated_ids(session, records, datenraum_nodes):
-    stored_serlo_org_ids = set(
-        node["externalId"]
-        for node in datenraum_nodes.values()
-        if not node["externalId"].startswith("https://datenraum.serlo.org/")
-    )
+    stored_serlo_org_ids = set(node["externalId"] for node in datenraum_nodes.values())
     current_ids = set(record["id"] for record in records)
 
     for ressource_id in stored_serlo_org_ids - current_ids:
